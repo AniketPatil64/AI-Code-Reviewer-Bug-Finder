9cb458091a7911a7473a8c42f386b82f175534b8
Aniket Patil <aniketpatil6400@gmail.com>
2025-12-09 12:15:59 +0530

    feat: added routes

---
+++ frontend/src/app/dashboard/page.tsx
@@ -0,0 +1,5 @@
+const Dashboard = () => {
+  return <div>Dashboard Hello</div>;
+};
+
+export default Dashboard;
---
+++ frontend/src/app/login/page.tsx
@@ -0,0 +1,5 @@
+const Login = () => {
+  return <div>Login Hello</div>;
+};
+
+export default Login;
