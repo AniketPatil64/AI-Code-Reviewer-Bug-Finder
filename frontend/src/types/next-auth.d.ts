a855c962dc096d9b5377cbfd7304475d5b43b716
Aniket Patil <aniketpatil6400@gmail.com>
2025-12-17 14:16:15 +0530

    feat: complted github login

---
+++ frontend/src/types/next-auth.d.ts
@@ -0,0 +1,16 @@
+import NextAuth from "next-auth";
+
+declare module "next-auth" {
+  interface Session {
+    user: {
+      provider?: string;
+    } & DefaultSession["user"];
+  }
+}
+
+declare module "next-auth/jwt" {
+  interface JWT {
+    provider?: string;
+    accessToken?: string;
+  }
+}
