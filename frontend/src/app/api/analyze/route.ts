385235220a8179ab2bed50297dda8197b007cf71
Aniket Patil <107613570+AniketPatil64@users.noreply.github.com>
2025-12-19 16:33:03 +0530

    Feat/ai module implmentation (#40)
    * feat: added dropdown text color
    
    * feat: added google genai module

---
+++ frontend/src/app/api/analyze/route.ts
@@ -0,0 +1,37 @@
+import { GoogleGenAI } from "@google/genai";
+
+const ai = new GoogleGenAI({ apiKey: "AIzaSyCxuczssIlREmHf2-sNMUkk4lJ2uSQvff8" });
+
+
+export async function POST(req: Request) {
+  try {
+    const { prompt } = await req.json();
+
+    if (!prompt) {
+      return Response.json({ error: 'Prompt missing' }, { status: 400 });
+    }
+    const response = await ai.models.generateContent({
+    model: "gemini-2.5-flash",
+    contents: prompt,
+  });
+ const rawText = response.text;
+
+  let parsed;
+  try {
+    parsed = JSON.parse(rawText || '');
+  } catch (e) {
+    console.error("Invalid JSON from AI:", rawText);
+    return Response.json(
+      { error: "AI returned invalid JSON" },
+      { status: 500 }
+    );
+  }
+  
+  return Response.json(parsed, { status: 200 });
+
+  } catch (err) {
+    console.error(err);
+    return Response.json({ error: 'Internal error' }, { status: 500 });
+  }
+}
+
