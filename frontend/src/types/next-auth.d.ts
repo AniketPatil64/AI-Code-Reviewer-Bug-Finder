a3de698b49bce70dbfce988da8a0f9e574821eeb
Aniket Patil <aniketpatil6400@gmail.com>
2025-12-22 18:24:55 +0530

    fix: auth jwt

--- frontend/src/types/next-auth.d.ts
+++ frontend/src/types/next-auth.d.ts
@@ -1,16 +1,19 @@
 import NextAuth from "next-auth";
 
 declare module "next-auth" {
+  interface User {
+    id: string;
+  }
+
   interface Session {
     user: {
-      provider?: string;
+      id: string;
     } & DefaultSession["user"];
   }
 }
 
 declare module "next-auth/jwt" {
   interface JWT {
-    provider?: string;
-    accessToken?: string;
+    id: string;
   }
 }
